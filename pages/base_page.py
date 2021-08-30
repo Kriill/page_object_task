f807ca75985114320bee5adedc71693ded5a7c19
Кирилл <kirill@Air-Kirill.megaseti.net>
2021-08-30 21:51:44 +0300

    parametrization

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,24 @@
+from selenium.common.exceptions import NoAlertPresentException # в начале файла
 from selenium.common.exceptions import NoSuchElementException
+from .locators import BasePageLocators
+import math
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def go_to_basket_page(self):
+        basket_link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
+        basket_link.click()
+    
+
+
 
     def open(self):
         self.browser.get(self.url)
@@ -15,3 +29,18 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
