638027ef4a7f12ada1dd739db71bee3228c8e392
Кирилл <kirill@Air-Kirill.megaseti.net>
2021-08-31 23:15:29 +0300

    final commit: class tests

--- pages/locators.py
+++ pages/locators.py
@@ -8,6 +8,10 @@ class MainPageLocators():
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, ".login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, ".register_form")
+    INPUT_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    INPUT_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REG_BUTTON = (By.NAME, "registration_submit")
 
 
 class BasePageLocators:
@@ -28,3 +32,4 @@ class ProductPageLocators:
 class BasketPageLocators:
     BASKET_ITEM = (By.CSS_SELECTOR, ".basket-items")
     BASKET_EMPTY_TEXT = (By.CSS_SELECTOR, "#content_inner>p")
+
--- test_product_page.py
+++ test_product_page.py
@@ -51,7 +51,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-@pytest.mark.new
+
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = MainPage(browser, link)
@@ -60,7 +60,7 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_no_product_in_basket()
 
-@pytest.mark.new
+
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -68,3 +68,51 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_no_product_in_basket()
+
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    MAINPAGE = "http://selenium1py.pythonanywhere.com/en-gb/"
+
+    def test_guest_can_go_to_login_page(self, browser):     
+        link = TestLoginFromMainPage.MAINPAGE
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        pass
+        link = TestLoginFromMainPage.MAINPAGE
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+
+@pytest.mark.user
+class TestUserAddToBasketFromProductPage:
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = str(time.time()) + "qwerty"
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        page = ProductPage(browser, link)
+        page.open()
+        product_page = ProductPage(browser, browser.current_url)
+        product_page.should_not_be_success_message()
+
+    @pytest.mark.new
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        page.basket()
